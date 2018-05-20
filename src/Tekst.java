bf3d9b8f280ea8ed77a64e909e0b3c0b5b98ff94
Mikolaj Klonowski <nick-93@o2.pl>
2018-05-20 11:55:07 +0200

    Zadanie z autami plus klasy z 2 obiektami plus tekst chociaż nie wiem czy dobrze

---
+++ src/Tekst.java
@@ -0,0 +1,20 @@
+public class Tekst {
+//    Klasa to szablon.
+//    Obiekt to konkretny egrzemplarz klasy i jest tworzony na podstawie schematu zawartego w klasie.
+//    Zmienna to wartość przypisana wg schematu klasy.
+//    Każda zmienna musi mieć określony typ danych, np. int, albo String.
+//    W klasie mogą się znajdować typy, nazwy i czynności do wykowania.
+//            Zmienne są wywoływane przez operator . i służą do tworzenia nowych obiektów.
+//    Jeśli w klasie zdefiniowano chociaż jeden parametr to kompilator nie wygeneruje dla nas obiektu domyślnego.
+//            Zakładając, że istnieje klasa Car, to przy zapisie:
+
+//    Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);
+//
+//    car1 nazywamy referencją,
+//
+//    a przy zapisie:
+//
+//    Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);  Car car2 = car1;
+//
+//    car1 I car2 to dwie referencje wskazujące na ten sam obiekt.
+}
